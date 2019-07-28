02233a40cb7a33409b085a966768c41b10bd25b0
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-07-28 11:05:43 -0400

    Logging Configuration

--- JavaLearning/hellomaven/src/main/java/academy/learnprogramming/HelloMaven.java
+++ JavaLearning/hellomaven/src/main/java/academy/learnprogramming/HelloMaven.java
@@ -1,7 +1,13 @@
 package academy.learnprogramming;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+    public final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
     public static void main(String[] args) {
         System.out.println("Hello Maven - Daily Commit");
+        log.info("Hello info");
+        log.debug("Hello Debug");
     }
 }
