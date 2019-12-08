582eb5ad820918044b827e23a6545a40de258f4f
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-12-07 22:13:02 -0500

    Counting Duplicates

--- JavaLearning/JavaPlayGround/src/PlayGround.java
+++ JavaLearning/JavaPlayGround/src/PlayGround.java
@@ -1,3 +1,28 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class PlayGround {
-    int firstint = 0;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    // Learn Java Streams
+
+    public static int DuplicateCount(String text){
+        HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
+        Arrays.stream(text.toLowerCase().split("")).forEach(l -> {
+            if(dictionary.containsKey(l)){
+                dictionary.put(l, dictionary.get(l) + 1);
+            } else {
+                dictionary.put(l, 1);
+            }
+        });
+        return (int)Arrays.stream(dictionary.keySet().toArray()).filter(k -> dictionary.get(k) > 1).count();
+    }
 }
+
+// shortcut
+
+// sout: System.out.println
