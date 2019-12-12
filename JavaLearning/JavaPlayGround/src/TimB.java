2e592d70f8d6155b673c138a599404682ed48d45
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-12-12 13:41:18 -0500

    Class2 Composition

--- JavaLearning/JavaPlayGround/src/TimB.java
+++ JavaLearning/JavaPlayGround/src/TimB.java
@@ -1,11 +1,26 @@
+import java.lang.management.MonitorInfo;
+
 public class TimB {
 
     public static void main(String[] args) {
 
-        Kia kia = new Kia(50 * 12);
-        kia.steer(25);
-        kia.accelerate(30);
-        kia.accelerate(20);
+        // Begin Composition
+        Dimensions dim = new Dimensions(20,20,5);
+        Case theCase = new Case("220B","Dell", "2400", dim);
+        Monitor theMonitor = new Monitor("27inc Beast", "Acer", 27, new Resolution(2540,1440));
+        Motherboard mb = new Motherboard("BJ-200", "Asus",4,6,"v3.69");
+        PC thePC = new PC(theCase, theMonitor, mb);
+
+        thePC.getMonitor().drawPixelAt(150,1200,"kia");
+        thePC.getMotherboard().loadProgram("Babberlicious Boo");
+        thePC.getTheCase().pressPower();
+
+        // End Composition
+
+//        Kia kia = new Kia(50 * 12);
+//        kia.steer(25);
+//        kia.accelerate(30);
+//        kia.accelerate(20);
 
 //        BankAccount czaraccount = new BankAccount("Czar",10_000);
 //        System.out.println("Current Balance: " + czaraccount.getBalance());
