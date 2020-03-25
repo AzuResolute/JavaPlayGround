d24791c6a8ade14730f8eedbcb90c04b61299174
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-03-25 09:36:47 -0400

    Sort Classes - Review

--- JavaLearning/JavaPlayGround/src/SortAlgo.java
+++ JavaLearning/JavaPlayGround/src/SortAlgo.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class SortAlgo {
     public static void main(String[] args) {
+        System.out.println("Hello");
+        BubbleSort.result();
+    }
+}
 
+class BubbleSort extends Sort {
+
+}
+
+abstract class Sort {
+    public static int[] sample = new int[]{1, 3, 2, 4, 7, 9, 13, 8};
+
+    public static int sort (int a, int b) {
+        return a - b;
+    }
+
+    public static void result () {
+        int[] temp = sample;
+        Arrays.sort(temp);
+        Arrays.stream(sample).forEach(System.out::println);
     }
 }
