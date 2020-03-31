2b64bbef96106b3e7c3929945cbff5cf35dede5d
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-03-31 14:44:54 -0400

    Reviewing BST

---
+++ JavaLearning/JavaPlayGround/src/BinarySearchTree.java
@@ -0,0 +1,14 @@
+public class BinarySearchTree {
+    private Node root = null;
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+}
